5c4e61918da00cef49a749ad93d3b1cc5a7f0b66
HERNAN PORMACHI QUICHCA <hernan.pormachi.quichca@everis.com>
2022-02-10 10:13:13 -0500

    add modules material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import { useRouter } from "next/router";
 
 function App() {
+  // const router = useRouter();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +12,14 @@ function App() {
         <p>
           Hola mundo <code>src/App.tsx</code> este es un demo nada mas.
         </p>
+        <button
+          onClick={() => {
+            console.log("ss");
+          }}
+        >
+          {"Siguiente"}
+        </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
